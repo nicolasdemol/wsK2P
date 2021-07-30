23d172630ac06cf4400cd1166c2016e5c920b925
mindflix <nyco.demol@gmail.com>
2021-07-30 20:13:42 +0200

    home

--- server/src/components/Home/Section4.jsx
+++ server/src/components/Home/Section4.jsx
@@ -1,5 +1,5 @@
 import ReactPlayer from "react-player";
-import { Card, CardMedia, Grid, makeStyles, Paper } from "@material-ui/core";
+import { makeStyles, Paper } from "@material-ui/core";
 import Sensor from "../Sensor";
 
 const useStyles = makeStyles((theme) => ({
