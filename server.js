3fcc7e61baee8e32b09b94e3f40354f611a91a18
mindflix <nyco.demol@gmail.com>
2023-04-12 15:25:45 +0200

    edit

--- server.js
+++ server.js
@@ -1,38 +1,18 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
-const https = require("https");
 const fs = require("fs");
+const PORT = 3000
+
 const app = express();
 
-// Certificate
-const privateKey = fs.readFileSync("./cert/key.pem", "utf8");
-const certificate = fs.readFileSync("./cert/cert.pem", "utf8");
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  passphrase: "poplpo",
-};
-
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80, () => {
-  console.log("HTTP Server running n port 80");
-});
-
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server running on port 443");
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.join(__dirname, "./build")));
 
 // GET Data (name, img) related to datecode (tra)
 app.get("/api/controls/:tra", (req, res, next) => {
-  tra = req.params.tra;
+  let tra = req.params.tra;
   fs.readFile(
     path.join(__dirname, `./api/QUALITE/Tracabilite/${tra}.tra`),
     "utf-8",
@@ -40,7 +20,7 @@ app.get("/api/controls/:tra", (req, res, next) => {
       if (!err) {
         const arr = [];
         data = data.replace(/[\s]+/gm, " ");
-        arrayTra = data.split(" ");
+        let arrayTra = data.split(" ");
 
         for (
           let i = 0, x = 0;
@@ -61,7 +41,7 @@ app.get("/api/controls/:tra", (req, res, next) => {
 
 // GET Image (img) for specific datecode (tra)
 app.get("/api/controls/:tra/file/:img", (req, res) => {
-  img = req.params.img;
+  let img = req.params.img;
   const imgFolder = img.substring(0, 3);
   const filepath = path.join(
     __dirname,
@@ -73,7 +53,7 @@ app.get("/api/controls/:tra/file/:img", (req, res) => {
 
 // GET Control File for datecode (tra)
 app.get("/api/controls/:tra/file", (req, res) => {
-  tra = req.params.tra;
+  let tra = req.params.tra;
   const filepath = path.join(
     __dirname,
     `./api/QUALITE/Resultat pour client/${tra}.txt`
